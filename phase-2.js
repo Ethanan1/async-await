baba705262d12acc68060b7b7199039d09dd0b6b
Ethanan1 <98491284+Ethanan1@users.noreply.github.com>
2022-08-09 20:56:39 -0700

    solved

--- phase-2.js
+++ phase-2.js
@@ -3,7 +3,7 @@ function stretch(timeLeft) {
     if (timeLeft < 1000) {
 
       // if we dont have enough time to complete the action
-      // reject the promise with the reason 
+      // reject the promise with the reason
       reject('you dont have enough time to stretch')
 
     } else {
@@ -57,12 +57,17 @@ function liftWeights(timeLeft) {
 
 // refactor this function to handle Promises using async/await instead of
   // .then and .catch
-function workout(totalTime) {
-  stretch(totalTime)
-    .then(timeLeftAfterStretching => runOnTreadmill(timeLeftAfterStretching))
-    .then(timeLeftAfterRunning => liftWeights(timeLeftAfterRunning))
-    .then(res => console.log(`done working out with ${res/1000} seconds left`))
-    .catch(err => console.log('Error: ', err));
+async function workout(totalTime) {
+  try {
+    const stretchTimeLeft = await stretch(totalTime);
+    const runTimeLeft = await runOnTreadmill(stretchTimeLeft);
+    const liftTimeLeft = await liftWeights(runTimeLeft);
+
+    console.log(`done working out with ${liftTimeLeft/1000} seconds left`);
+
+  } catch (error) {
+    console.log("Error: ", error)
+  }
 }
 
 
